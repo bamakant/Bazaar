5f43f6343f2f7623fc1a8e9f1c957fd8e0d67de7
bamakant <bamakantkar@gmail.com>
2017-08-03 01:37:22 +0530

    Initial commit

---
+++ app/src/main/java/com/kiusoftech/bazaar/ExportFragment.java
@@ -0,0 +1,27 @@
+package com.kiusoftech.bazaar;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ExportFragment extends Fragment {
+
+    public ExportFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_export, container, false);
+    }
+
+}
